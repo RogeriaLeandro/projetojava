e7aceb70b0da76f987d9f9c8878426018b09df0f
RogeriaLeandro <rogeriaaraujoleandro@gmail.com>
2020-10-09 11:15:21 -0300

    Criação do Projeto

---
+++ projjava/src/main/java/br/com/projetojava/App.java
@@ -0,0 +1,13 @@
+package br.com.projetojava;
+
+/**
+ * Olá, estou entrando em campo!!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Olá, estou entrando em campo!" );
+    }
+}
